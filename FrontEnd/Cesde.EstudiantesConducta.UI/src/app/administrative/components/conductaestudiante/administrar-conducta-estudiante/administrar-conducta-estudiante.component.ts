2045cfb454ed1735ca35fa01504a8bd541f0225a
David garcia <garciavelez2009@hotmail.com>
2024-01-01 23:17:29 -0500

    Actualizate

--- FrontEnd/Cesde.EstudiantesConducta.UI/src/app/administrative/components/conductaestudiante/administrar-conducta-estudiante/administrar-conducta-estudiante.component.ts
+++ FrontEnd/Cesde.EstudiantesConducta.UI/src/app/administrative/components/conductaestudiante/administrar-conducta-estudiante/administrar-conducta-estudiante.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { conductaEstudiante } from 'src/app/administrative/models/conductaEstudiante';
+import { ConductaEstudianteService } from 'src/app/administrative/services/conducta-estudiante.service';
+
 
 @Component({
   selector: 'app-administrar-conducta-estudiante',
@@ -6,5 +9,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./administrar-conducta-estudiante.component.css']
 })
 export class AdministrarConductaEstudianteComponent {
+
+  conductaEstudiante: conductaEstudiante[] = [];
+
+
+  constructor(
+    private_conductaestudianteservice: ConductaEstudianteService,
+    
+  ) {
+
+
+  }
 
 }
